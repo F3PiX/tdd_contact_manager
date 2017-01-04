2232b109d50138bbbe2822f15555f6143e84b876
F3PiX <F3PiXnl@gmail.com>
2017-01-04 13:32:07 +0100

    Update tests

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -1,5 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe Person, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+
+  let(:person) do
+    Person.new(first_name: 'Johan', last_name: 'Cruyff')
+  end
+
+  it 'has a valid test person' do
+    expect(person).to be_valid
+  end
+
+  it 'is invalid without a first name' do
+    person.first_name = nil
+    expect(person).not_to be_valid
+  end
+
+# and what I can think of
+   it 'is invalid without a last name ' do
+     person.last_name = nil
+     expect(person).not_to be_valid
+   end
+
+  # I don't need this test, because the shared person is a valid instance
+  # it 'is valid with a first  and a last name' do
+  #   # skip
+  #   person = Person.new(first_name: 'Mini', last_name: 'Verstappen')
+  #   expect(person).to be_valid
+  # end
+
+
+
+
+
+
+
+  end
